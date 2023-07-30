be99c6bcf6a57dfa3b1967d6821c590cfa9b0742
kenan551 <kenan.ramazanov1104@gmail.com>
2023-07-31 00:56:00 +0400

    navbar last-version

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Navbar.module.css'
 import {AiOutlineMenu, AiOutlineClose, AiOutlineSearch, AiOutlineUser} from
  'react-icons/ai'
  import Logo from '../../images/logo.png'
 const Navbar = () => {
+
+
+    const [nav,setNav] = useState(false)
   return (
     <header className={styles.navbar}>
  <img src={Logo} alt="/" />
  <nav>
-    <ul className={styles.menu}>
+    <ul className={nav ? [styles.menu, styles.active].join(' ') : [styles.menu]}>
         <li>
             <a href="/">Learn More</a>
         </li>
@@ -19,13 +22,19 @@ const Navbar = () => {
             <a href="/">Sign up</a>
         </li>
         <li>
-            <AiOutlineSearch></AiOutlineSearch>
+            <AiOutlineSearch size={25} style={{marginTop: '6px'}}></AiOutlineSearch>
         </li>
         <li>
-            <AiOutlineUser></AiOutlineUser>
+            <AiOutlineUser size={25} style={{marginTop: '6px'}} ></AiOutlineUser>
         </li>
+    
     </ul>
  </nav>
+ <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
+         {nav ? <AiOutlineClose size={25}></AiOutlineClose> :  <AiOutlineMenu size={25}></AiOutlineMenu>}
+            
+           
+        </div>
     </header>
   )
 }
