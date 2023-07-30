1c91cd8a9d471132b68fe5cc83a9db21a8201b68
kenan551 <kenan.ramazanov1104@gmail.com>
2023-07-31 00:01:26 +0400

    creating navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../public/vite.svg'
+import Navbar from './components/navbar/Navbar';
+function App() {
+  return (
+    <div>
+    <Navbar></Navbar>
+      </div>
+  )
+}
+
+export default App;
