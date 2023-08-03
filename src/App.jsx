7676171bb012b1b5b981bd93df51005b5bfa36be
kenan551 <kenan.ramazanov1104@gmail.com>
2023-08-03 16:43:15 +0400

    swiper part

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import logo from '../public/vite.svg'
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Find from './components/find/Find';
 function App() {
   return (
     <div>
     <Navbar></Navbar>
     <Hero></Hero>
+    <Find></Find>
       </div>
   )
 }
